b66ea550991c7c622ebe6a67a55ca1652b7d7a59
Md. Abdullah Ibna Harun <abdullah13426.cse@gmail.com>
2021-12-07 23:26:02 +0600

    some bug fixed

---
+++ src/test/java/com/abdullah/main/EQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

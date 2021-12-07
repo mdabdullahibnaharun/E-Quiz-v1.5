fe243564ac9bd5cfe134c8865ff2352a0220a01e
Md. Abdullah Ibna Harun <abdullah13426.cse@gmail.com>
2021-12-07 23:01:26 +0600

    Add files via upload

---
+++ src/main/java/com/tf4/main/repository/ResultRepo.java
@@ -0,0 +1,11 @@
+package com.tf4.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tf4.main.model.Result;
+
+@Repository
+public interface ResultRepo extends JpaRepository<Result, Integer> {
+	
+}
